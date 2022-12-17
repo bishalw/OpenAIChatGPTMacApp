ecac192b9bbbb9dd6fa1169b1921b1f778d221a6
Bishal Wagle <bishalw@gmail.com>
2022-12-17 11:16:56 -0800

    Alpha 0.1.0 ChatGPT Open AI MacOS item bar

--- ChatGPTMac/View/ChatGPTView.swift
+++ ChatGPTMac/View/ChatGPTView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  ChatGPTView.swift
 //  ChatGPTMac
 //
 //  Created by Bishalw on 12/17/22.
@@ -7,14 +7,14 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ChatGPTView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct ChatGPTView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ChatGPTView()
     }
 }
--- ChatGPTMac/View/WebView.swift
+++ ChatGPTMac/View/WebView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  WebView.swift
 //  ChatGPTMac
 //
 //  Created by Bishalw on 12/17/22.
@@ -7,14 +7,14 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct WebView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct WebView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        WebView()
     }
 }
