fa78119a81966d3715a594921a6a64ba57b2be39
Bishal Wagle <bishalw@gmail.com>
2022-12-17 06:50:31 -0800

    Initial Commit

---
+++ ChatGPTMac/ChatGPTMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTMacApp.swift
+//  ChatGPTMac
+//
+//  Created by Bishalw on 12/17/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
